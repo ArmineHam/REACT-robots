7e26b7112136ab2169f7c28c068f25cb46ed6c84
ArminHam <arminehambardzumyan@yahoo.com>
2022-06-09 15:33:37 +0200

    initials

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsac",
+    username: "Karianne",
+    email: "JuliannaOconner@kory.org",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lusio-Hammington@annie.ca",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulits",
+    username: "Leopoldo-Corkery",
+    email: "Karley-Dach@jasper.info",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 8,
+    name: "Lizzy Mad",
+    username: "Mancho",
+    email: "Sincere@cool.biz",
+  },
+  {
+    id: 9,
+    name: "HAIdi",
+    username: "Beard",
+    email: "Bon@april.biz",
+  },
+  {
+    id: 10,
+    name: "Lee Gra",
+    username: "Gros",
+    email: "Sincere@pet.biz",
+  },
+];
